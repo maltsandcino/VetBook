66bea9f3d8a30ac8d75650aba88c3477e1864736
Tom <thomasjh@gmail.com>
2024-02-18 10:36:55 +0100

    navbar and layout established, css and js referenced, a bit of logic for login done

--- VetBooker/views.py
+++ VetBooker/views.py
@@ -21,43 +21,45 @@ def login_view(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "VetBooker/login.html", {
-                "message": "Invalid username and/or password."
+                "message": "Invalid username and/or password. Please re-enter your details, or contact your systems administrator"
             })
     else:
         return render(request, "VetBooker/login.html")
 
 def logout_view(request):
-  
+
     if request.user.is_authenticated:
         logout(request)
     return HttpResponseRedirect(reverse("index"))
 
-
+###Only superusers may create other users in the system via the register page.
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+    if request.user.is_superuser:
+        if request.method == "POST":
+            username = request.POST["username"]
+            email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
+            # Ensure password matches confirmation
+            password = request.POST["password"]
+            confirmation = request.POST["confirmation"]
+            if password != confirmation:
+                return render(request, "network/register.html", {
+                    "message": "Passwords must match."
+                })
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+            # New user is created, and handled if the username already exists
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+            except IntegrityError:
+                return render(request, "network/register.html", {
+                    "message": "Username already taken."
+                })
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/register.html")
     else:
-        return render(request, "network/register.html")
+            return HttpResponseRedirect(reverse("index"))
 
 def index(request):
 

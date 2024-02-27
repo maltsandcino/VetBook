578b9289638277c028b1ad9f6b87ffab29e5c78f
Tom <thomasjh@gmail.com>
2024-02-27 12:14:49 +0100

    Styles, initial script for management, some html.

--- VetBooker/static/VetBooker/script.js
+++ VetBooker/static/VetBooker/script.js
@@ -1,4 +1,23 @@
 // Fetch data for booking, to see what the soonest availability is based on specialization. This data will be drawn from the django form.
+document.addEventListener('DOMContentLoaded', () =>{
+    ;
+})
+
+function manageCustomer() {
+    let managementDiv = document.querySelector("#managementDiv");
+    managementDiv.classList.toggle('notVisible');
+    const customerSearch = document.createElement('div')
+    customerSearch.innerHTML = `<fieldset><label for="numberSearch">For existing customers, enter their telephone number:</label>
+    <input type="text" id="numberSearch" name="numberSearch"> <button id="customerEdit">Search</button>
+    </fieldset>`
+    managementDiv.append(customerSearch)
+    document.getElementById('customerEdit').addEventListener('click', manageCustomerSearch)
+}
+
+function manageCustomerSearch() {
+    let clientNumber = document.getElementById('numberSearch').value;
+    console.log(clientNumber);
+}
 
 function submitBooking() {
   
